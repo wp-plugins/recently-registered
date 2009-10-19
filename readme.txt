b432af2f3d3037fb8d5bc9458653b78ecee40c3e
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-19 14:02:25 +0000

    version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/recently-registered/trunk@164858 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Tags: users, recent, new
 Contributors: ipstenu
 Requires at least: Unknown
-Tested up to: 2.8
-Stable Tag: 1.0
+Tested up to: 2.8.4
+Stable Tag: 1.1
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5227973
 
 All this does is add in a submenu under the users menu on the admin side for recently registered users.
@@ -12,40 +12,44 @@ All this does is add in a submenu under the users menu on the admin side for rec
 
 With this plugin, admins will have a new submenu under the Users menu to list the 25 most recently registered users (in reverse order), pulling down their avatar, based on what you set as default (so identicon, gravatar, etc). 
 
-==Changelog==
-=  1.0 =
+= Version 1.1 =
+**16 October, 2009 **
+
+* Added in comment count to page.
+* Added option to change recent number from 25 to whavever you want.
+
+= Version 1.0 =
 **01 May, 2009**
 
 * Removed the since code (it wasn't working) and replaced with a short date.
 
-=  0.2 =
+= Version 0.2 =
 **30 March, 2009**
 
 * Moved to a sub-folder
 * Formatting the list to be nicer.
 
-=  0.1 =
+= Version 0.1 =
 **27 March, 2009**
 
-* Initial version.
+Initial version.
 
 == Installation ==
 
 1. Unpack the *.zip file and extract the `/recently-registered/` folder and the files.
 2. Using an FTP program, upload the full `/recently-registered/` folder to your WordPress plugins directory (Example: `/wp-content/plugins/`).
-3. Go to **Plugins > Installed** and activate the plugin.
+3. Go to Plugins > Installed and activate the plugin.
 4. Go to **Users > Recently Registered** and see who's new.
 
 == To Do ==
 
-1. Add in a delete user link (have to sort out the nonce stuff for that, and if there was a delete option from a users wp profile, I'd be done).
-2. Maybe make the 25 recent users a selectable list. 5, 10 or 25.
+1. Add in a delete user link (have to sort out the nonce stuff for that).
 
 == Frequently Asked Questions ==
 
 = Will this work on older versions of WordPress? =
 
-Probably, but I only have v 2.7.1.  Just at a glance, it should work on any 2.x WP at this point.
+Probably, but I started writing this on v 2.7.1.  Just at a glance, it should work on any 2.x WP at this point. That said, you should ALWAYS upgrade, folks!
 
 = Will this work if I have a special avatar plugin? =
 
--- recently-registered.php
+++ recently-registered.php
@@ -3,14 +3,11 @@
 Plugin Name: Recently Registered
 Plugin URI: http://www.ipstenu.org/
 Description: All this does is add in a submenu under the users menu on the admin side for recently registered users.
-Version: 1.0
+Version: 1.1
 Author: Mika Epstein
 Author URI: http://www.ipstenu.org/
 
         Copyright 2009 Mika Epstein (email: ipstenu@ipstenu.org)
-
-        wp_since was stolen from bb_since from bbPress.  It worked so nicely,
-        I didn't want to reinvent the wheel!
 
         This plugin is free software; you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
@@ -28,54 +25,26 @@ Author URI: http://www.ipstenu.org/
 
 function recentlyregistered_menu() {
                 if (function_exists('add_submenu_page')) {
-                add_submenu_page('users.php', 'Recently Registered', 'Recently Registered', '8', 'recently-registered.php', 'recentlyregistered_options');
+                add_submenu_page('users.php', 'Recently Registered', 'Recently Registered', '8', 'recently-registered/recently-registered_options.php');
         }
 }
 
-function recentlyregistered_options() {
-        global $wpdb;
-        $aUsersID = $wpdb->get_col( $wpdb->prepare("SELECT $wpdb->users.ID FROM $wpdb->users ORDER BY ID DESC LIMIT 25"));
-        $size = 40;
-        $default = $wpdb->get_var("SELECT option_value FROM $wpdb->options WHERE option_name='avatar_default'");
+// Adding in an option for Stop Forum Spam
+function recentlyregistered_activate() {
+        update_option('recentlyregistered_stopforumspam', '0');
+        updatee_option('recentlyregistered_number', '25');
+}
 
-?>
-<div class="wrap">
-<h2>25 Most Recently Registered Users</h2>
-
-<table class="widefat fixed" cellspacing="0">
-<thead>
-<tr class="thead">
-        <th scope="col" id="username" class="manage-column column-username" style="">Username</th>
-        <th scope="col" id="email" class="manage-column column-email" style="">E-mail</th>
-        <th scope="col" id="registered" class="manage-column column-registered" style="">Registered</th>
-</tr>
-</thead>
-
-<tbody id="users" class="list:user user-list">
-<?php
-
-        foreach ( $aUsersID as $iUserID ) :
-                $user = get_userdata( $iUserID );
-                $email = $user->user_email;
-                $registered = strtotime($user->user_registered);
-                $grav_url = "http://www.gravatar.com/avatar/".md5( strtolower($email) )."?d=".$default."&size=".$size;
-                ?>
-
-                <tr id="<?php echo $iUserID; ?>">
-                  <td class="username column-username"><img alt="Avatar" src="<?php echo $grav_url; ?>" class="avatar avatar-<?php echo $size; ?> photo" height="<?php echo $size; ?>" width="<?php echo $size; ?>" /> <strong><a href="user-edit.php?user_id=<?php echo $user->ID ?>;wp_http_referer=%2Fblog%2Fwp-admin%2Fusers.php"><?php echo $user->user_login; ?></a></strong><br /><div class="row-actions"><span class='edit'><a href="user-edit.php?user_id=<?php echo $user->ID ?>&#038;wp_http_referer=%2Fblog%2Fwp-admin%2Fusers.php">Edit</a></span></div></td>
-                  <td class="email column-email"><a href="mailto:<?php echo $email; ?>" title="e-mail: <?php echo $email; ?>"><?php echo $email; ?></a></td>
-                  <td class="registered column-registered"><?php echo date('d M \- g:h:s a', $registered); ?></td>
-                </tr>
-                <?php
-        endforeach;
-?>
-</table>
-
-</div>
-<?php
+// Delete the options if the plugin is being turned off (pet peeve)
+function recentlyregistered_deactivate() {
+        delete_option('recentlyregistered_stopforumspam');
+        delete_option('recentlyregistered_number');
 }
 
 // Hooks
 add_action('admin_menu', 'recentlyregistered_menu');
+
+register_activation_hook( __FILE__, 'recentlyregistered_activate' );
+register_deactivation_hook( __FILE__, 'recentlyregistered_deactivate' );
 
 ?>
