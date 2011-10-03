cab72801715a8ec9fe908c8b1355c49f87bd1760
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-03 19:38:43 +0000

    GPL2
    git-svn-id: https://plugins.svn.wordpress.org/recently-registered/trunk@446883 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- recently-registered.php
+++ recently-registered.php
@@ -13,7 +13,7 @@ Copyright 2009-11 Mika Epstein (email: ipstenu@ipstenu.org)
 
     Recently Registered is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
+    the Free Software Foundation, either version 2 of the License, or
     (at your option) any later version.
 
     Recently Registered is distributed in the hope that it will be useful,
